2787eef2ae5f25c2f085ffd6a8821ca0e67b9145
DESKTOP-TG3F8QK\Administrator <d1i2n3g4@qq.com>
2023-07-18 23:15:13 +0800

    提交 鼠标设置

--- Assets/Scripts/MouseWorld.cs
+++ Assets/Scripts/MouseWorld.cs
@@ -1,8 +1,24 @@
+using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Vector3 = UnityEngine.Vector3;
 public class MouseWorld : MonoBehaviour
 {
-   
+    private static MouseWorld instance;
+
+    [SerializeField] private LayerMask mousePlaneLayerMask;
+   /// <summary>
+   /// Update is called every frame, if the MonoBehaviour is enabled.
+   /// </summary>
+   private void Update()
+   {
+        transform.position = MouseWorld.GetPosition();
+   }
+
+   public static Vector3 GetPosition(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.mousePlaneLayerMask);
+        return raycastHit.point;
+    }
 }
